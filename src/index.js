3ef2b93fc7b85928d3d3b780902dada79a4f7f5e
Shehan Gunasekara <87413135+it21011870@users.noreply.github.com>
2022-08-23 16:05:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import react from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite food</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
